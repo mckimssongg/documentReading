fe9f27cb80a3c8b813e8b0f3eef4f8187a18c968
mckimssongg <soymckimsson286@gmail.com>
2023-02-24 21:27:40 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace documentReading
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataView = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(45, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Subir spreadsheets";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_load);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(398, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(437, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Visualizador de spreadsheets";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dataView
+            // 
+            this.dataView.AllowUserToOrderColumns = true;
+            this.dataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataView.Location = new System.Drawing.Point(299, 49);
+            this.dataView.Name = "dataView";
+            this.dataView.ReadOnly = true;
+            this.dataView.Size = new System.Drawing.Size(648, 593);
+            this.dataView.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(45, 329);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(206, 36);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Vista por grupo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(45, 126);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(206, 36);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Vista completa";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(45, 414);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(206, 36);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Vista por año";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(86, 228);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(114, 36);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Buscar";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(45, 185);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(206, 24);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "nombre";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(968, 654);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataView);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Home";
+            this.Text = "Visualizador de spreadsheets";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataView;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using DataTable = System.Data.DataTable;
+
+namespace documentReading
+{
+    public partial class Home : Form
+    {
+
+        public DataTable dt = new DataTable();
+        public DataTable dataTableGroup = new DataTable();
+        public Dictionary<string, double > totalByGroup = new Dictionary<string, double >();
+        public List<string> stringsGroups = new List<string>();
+
+        public DataTable dataTableAge = new DataTable();
+        public Dictionary<string, double> totalByAge = new Dictionary<string, double>();
+        public List<string> stringsAge = new List<string>();
+
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_load(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Archivos de Excel|*.xlsx;*.xls|Todos los archivos|*.*";
+            openFileDialog.Title = "Selecciona un archivo de Excel";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog.FileName;
+
+                // Abre el archivo de Excel
+                Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+                Workbook workbook = excel.Workbooks.Open(fileName);
+                Worksheet worksheet = workbook.ActiveSheet;
+
+                // Lee los datos de las celdas
+                Range range = worksheet.UsedRange;
+
+                for (int j = 1; j <= range.Columns.Count; j++)
+                {
+                    // Agrega el nombre de cada columna a la tabla
+                    string columnName = ((Range)range.Cells[1, j]).Value2.ToString();
+                    dt.Columns.Add(columnName, typeof(string));
+
+                }
+
+                for (int i = 2; i <= range.Rows.Count; i++)
+                {
+                    // Agrega cada fila a la tabla
+                    DataRow row = dt.NewRow();
+                    for (int j = 1; j <= range.Columns.Count; j++)
+                    {
+                        
+                        if ( j == 9 )
+                        {
+                            // Lee el valor de la celda y lo agrega a la fila
+                            string cellValue = ((Range)range.Cells[i, j]).Value2.ToString();
+                            row[j - 1] = cellValue;
+
+                            if (totalByGroup.ContainsKey(cellValue))
+                            {
+                                double venta = (((Range)range.Cells[i, j + 1]).Value2);
+                                totalByGroup[cellValue] += venta;
+                            }
+                            else
+                            {
+                                double venta = (((Range)range.Cells[i, j + 1]).Value2);
+                                totalByGroup.Add(cellValue, venta);
+                                stringsGroups.Add(cellValue);
+                            }
+                        }
+                        else if ( j == 8)
+                        {
+                            // Lee el valor de la celda y lo agrega a la fila
+                            double cellValue = ((Range)range.Cells[i, j]).Value2;
+                            DateTime dateTime = DateTime.FromOADate(cellValue);
+                            string data = dateTime.ToString("dd/MM/yyyy HH:mm:ss");
+                            row[j - 1] = data;
+
+                            string age = dateTime.Year.ToString();  
+                            if (totalByAge.ContainsKey(age))
+                            {
+                                double venta = (((Range)range.Cells[i, j + 2]).Value2);
+                                totalByAge[age] += venta;
+                            }
+                            else
+                            {
+                                double venta = (((Range)range.Cells[i, j + 2]).Value2);
+                                totalByAge.Add(age, venta);
+                                stringsGroups.Add(age);
+                            }
+                        }
+                        else if ( j == 3)
+                        {
+                            // Lee el valor de la celda y lo agrega a la fila
+                            double cellValue = ((Range)range.Cells[i, j]).Value2;
+                            DateTime dateTime = DateTime.FromOADate(cellValue);
+                            string data = dateTime.ToString("dd/MM/yyyy HH:mm:ss");
+                            row[j - 1] = data;
+                        }
+                        else
+                        {
+
+                            // Lee el valor de la celda y lo agrega a la fila
+                            string cellValue = ((Range)range.Cells[i, j]).Value2.ToString();
+                            row[j - 1] = cellValue;
+
+                        }
+
+                    }
+                    dt.Rows.Add(row);
+
+                    // Asigna la tabla al componente dataView
+                    dataView.DataSource = dt;
+                }
+
+                // Cierra el archivo de Excel
+                workbook.Close();
+                excel.Quit();
+
+                MessageBox.Show("Finalizado con exito :))))))");
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataTableGroup.Rows.Count > 0)
+            {
+                dataView.DataSource = dataTableGroup;
+            }
+            else
+            {
+                dataTableGroup.Columns.Add("Grupo", typeof(string));
+                dataTableGroup.Columns.Add("Total ventas", typeof(double));
+
+                foreach (KeyValuePair<string, double> entry in this.totalByGroup)
+                {
+                    DataRow row = dataTableGroup.NewRow();
+                    row[0] = entry.Key;
+                    row[1] = entry.Value;
+                    dataTableGroup.Rows.Add(row);
+                }
+
+                dataView.DataSource = dataTableGroup;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataView.DataSource = dt;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (dataTableAge.Rows.Count > 0)
+            {
+                dataView.DataSource = dataTableAge;
+            }
+            else
+            {
+                dataTableAge.Columns.Add("Año", typeof(string));
+                dataTableAge.Columns.Add("Total ventas", typeof(double));
+
+                foreach (KeyValuePair<string, double> entry in this.totalByAge)
+                {
+                    DataRow row = dataTableAge.NewRow();
+                    row[0] = entry.Key;
+                    row[1] = entry.Value;
+                    dataTableAge.Rows.Add(row);
+                }
+
+                dataView.DataSource = dataTableAge;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string textSearch = textBox1.Text;
+            textBox1.Clear();
+
+            if (textSearch.Length > 0)
+            {
+                // Nombre de la columna en la que buscar
+                string nameColumFilter = "Nombre";
+
+                // Verificar si la columna existe en la tabla
+                if (!dt.Columns.Contains(nameColumFilter))
+                {
+                    MessageBox.Show($"La columna {nameColumFilter} no existe en la tabla");
+                    return;
+                }
+
+                // Buscamos en la columna indicada los datos que contengan el texto introducido en el textbox
+                DataRow[] rows = dt.Select($"{nameColumFilter} LIKE '%{textSearch}%'");
+
+                // Crear la tabla que se mostrará en el DataView
+                DataTable dtSearch = new DataTable();
+
+                // Crear las columnas de la tabla
+                foreach (DataColumn col in dt.Columns)
+                {
+                    dtSearch.Columns.Add(col.ColumnName, col.DataType);
+                }
+
+                // Añadir las filas a la tabla de búsqueda
+                foreach (DataRow row in rows)
+                {
+                    dtSearch.ImportRow(row);
+                }
+
+                // Mostrar los datos en el DataView
+                dataView.DataSource = dtSearch;
+            }
+        }
+    }
+}
